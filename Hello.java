2b1fd3bdb08ca32ab49799109501b4bf4be76c79
BigPacker <57052920+BigPacker@users.noreply.github.com>
2022-09-29 12:18:55 +0100

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+  public static void main(String[] args) [
+  // displays "Hello Everyone - This is Peter's zeit8024 lab" in console
+  System.out.println("Hello Everyone - This is Peter's zeit8024 lab");
+  }
+}
